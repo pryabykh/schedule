f420a9fc9c0e12247286dfeb76a926275f4fdbe9
Pavel Ryabykh <pryabykh@gmail.com>
2022-06-27 22:37:18 +0300

    add migration for workspaces table

--- workspace-service/src/main/resources/db/changelog/versions/db.changelog-1.0.sql
+++ workspace-service/src/main/resources/db/changelog/versions/db.changelog-1.0.sql
@@ -2,6 +2,11 @@
 
 --changeset workspace:1
 CREATE TABLE workspaces (
-
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar(255) NOT NULL,
+    creator bigint REFERENCES users (id) ON DELETE SET NULL,
+    version integer NOT NULL,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL
 );
 --rollback drop table workspaces;
