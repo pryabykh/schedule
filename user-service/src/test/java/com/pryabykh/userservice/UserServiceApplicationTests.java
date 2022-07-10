febb9d16a19b0dd8da0eb5105e2c5fbdf0e48ecd
Pavel Ryabykh <pryabykh@gmail.com>
2022-07-10 13:42:15 +0300

    init entity service project

--- user-service/src/test/java/com/pryabykh/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/pryabykh/userservice/UserServiceApplicationTests.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 @SpringBootTest
 @ActiveProfiles("test")
 class UserServiceApplicationTests {
